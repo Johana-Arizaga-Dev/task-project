9b8a602f09e8e6f7cec6cb64c1503e069db2061b
Johana Lizbeth Arizaga Avalos <johana.arizaga.ti@gmail.com>
2026-01-15 00:34:38 -0600

    Refactor database configuration to use environment variables

--- backend/src/db.js
+++ backend/src/db.js
@@ -2,9 +2,10 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 export const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "tasks_db",
-  password: "1309",
-  port: 5432,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
+
